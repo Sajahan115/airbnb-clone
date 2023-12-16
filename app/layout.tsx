dc54214a22d98328f1dbab01e8763abec6f891c4
Sajahan115 <sajahankhan115@gmail.com>
2023-12-16 19:14:30 +0530

    First commit

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Nunito } from "next/font/google";
+
+import "./globals.css";
+import Navbar from "../components/navbar/Navbar";
+import RegisterModal from "@/components/modals/RegisterModal";
+
+const font = Nunito({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Airbnb",
+  description: "Airbnb Clone",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <RegisterModal />
+        <Navbar />
+        {children}
+      </body>
+    </html>
+  );
+}
